bff7b0febfc1a5d95f7c90233b1cf0714a8f4a0a
JeongHyeHa <hajeonghye@naver.com>
2024-04-29 13:29:33 +0900

    userpage_로그인 전_ 선

--- src/components/userpage/userpage.js
+++ src/components/userpage/userpage.js
@@ -1,9 +1,9 @@
-import React from 'react'; 
-import { Link } from 'react-router-dom'; 
-import profile from '../../assets/image/profile.jpg'; 
+import React from 'react';
+import { Link } from 'react-router-dom';
+import profile from '../../assets/image/profile.jpg';
 import './userpage.css';
 
-function UserPage () { 
+function UserPage() {
     return (
         <div>
             <header className="main-header">
@@ -13,11 +13,14 @@ function UserPage () {
                 <div className="profile-image-container">
                     <img src={profile} alt="로고" className="profile-image" />
                 </div>
-                <div className="title">
-                    <Link to="/login">Login</Link>
-                </div>
-                <div className="title">
-                    <Link to="/signup">Sign Up</Link>
+                
+                <div className="title-container">
+                    <div className="title top">
+                        <Link to="/login">Login</Link>
+                    </div>
+                    <div className="title bottom">
+                        <Link to="/signup">Sign Up</Link>
+                    </div>
                 </div>
             </main>
         </div>
