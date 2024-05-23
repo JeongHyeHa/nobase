c76acbcf4676fe2c553497ad8ef376e8cdec4492
JeongHyeHa <hajeonghye@naver.com>
2024-05-23 16:50:48 +0900

    userpage에 회원탈퇴 버튼 및 창 생성. 크기 조정 필요

--- src/components/userpage/withdraw.js
+++ src/components/userpage/withdraw.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import './withdraw.css';
+import { useCookies } from 'react-cookie';
+import Layout from '../layout/layout';
+import { ReactComponent as XClose } from '../../assets/icon/x.svg';
+import alertIcon from '../../assets/icon/alert.png';
 
 function Withdraw({ onClose }) {
   const [inputValue, setInputValue] = useState('');
   const [isChecked, setIsChecked] = useState(false);
+  const [cookies] = useCookies(['user']);
+  const userId = cookies.user?.id;
 
   const handleConfirmDelete = () => {
-    if (inputValue === '계정' && isChecked) {
+    if (inputValue === userId && isChecked) {
       alert('계정이 삭제되었습니다.');
-      // 여기서 계정 삭제 로직을 추가하세요.
+      // 여기서 계정 삭제 로직 추가
       onClose();
     } else {
       alert('계정 삭제 확인을 위해 필드를 올바르게 입력하고 체크박스를 선택해주세요.');
@@ -16,30 +22,38 @@ function Withdraw({ onClose }) {
   };
 
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <h2>정말로 계정을 삭제할까요?</h2>
-        <p>계정 삭제시 내가 참여중인 워크스페이스의 내 드라이브와 내 드라이브의 모든 항목이 삭제될 거에요. 또한, 내가 소유권을 넘긴 워크스페이스의 일부 항목은 삭제되지 않을 수 있습니다.</p>
-        <p>계정 삭제를 원하시면 아래에 '계정'을 입력해주세요.</p>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          placeholder="계정"
-        />
-        <div>
-          <input
-            type="checkbox"
-            checked={isChecked}
-            onChange={(e) => setIsChecked(e.target.checked)}
-          />
-          <label>내가 모든 데이터를 삭제되는 것에 동의합니다.</label>
+    <Layout>
+      <div className="modal">
+        <div className="modal-content">
+          <button className="x-button" onClick={onClose}><XClose /></button>
+          <div className="header">
+            <h1>정말로 계정을 삭제할까요?</h1>
+          </div>
+          <p className='alertContent'>
+            만약 계정을 삭제하면<b> 내 드라이브와 내 드라이브 하위의 모든 데이터가 영구적으로 삭제될 거예요.</b> 또한, 내가 소유권을 넘긴 워크스페이스의 일부 항목은 삭제되지 않을 수 있습니다.
+          </p>
+          <section className='input_userid'>
+            <h2>확인을 위해 나의 이메일을 입력해주세요</h2>
+            <div className='textfieldview'>
+              <label className='text_label'>나의 계정 입력</label>
+              <input placeholder={userId} className='text_input' 
+                value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+            </div>
+            <div className='checkboxview'>
+              <input type="checkbox" checked={isChecked} onChange={(e) => setIsChecked(e.target.checked)} />
+              <label className='checkbox_label'><span>나의 모든 데이터가 삭제되는 것을 이해했습니다.</span></label>
+            </div>
+          </section>    
+          <div className='statusMessage'>
+            <p className="warning"><img src={alertIcon} alt="alertIcon" className="alertIcon" /> 이 작업은 취소할 수 없습니다.</p>
+          </div>
+          <div className='footer'>
+            <button className="cancel-btn" onClick={onClose}>취소</button>
+            <button className="confirm-btn" onClick={handleConfirmDelete}>계정 삭제</button>
+          </div>
         </div>
-        <p className="warning">⚠️ 이 작업은 취소할 수 없습니다.</p>
-        <button className="cancel-button" onClick={onClose}>취소</button>
-        <button className="confirm-button" onClick={handleConfirmDelete}>계정 삭제</button>
       </div>
-    </div>
+    </Layout>
   );
 }
 
